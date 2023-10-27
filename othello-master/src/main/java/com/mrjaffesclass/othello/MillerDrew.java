32c70034f687df87c893cdddca9fb23aedf24ee0
Drew Miller <drew.miller@mrjaffesclass.com>
2023-10-27 12:39:36 -0700

    MillerDrew player class initial

---
+++ othello-master/src/main/java/com/mrjaffesclass/othello/MillerDrew.java
@@ -0,0 +1,25 @@
+package com.mrjaffesclass.othello;
+
+/**
+ * Test player
+ */
+public class MillerDrew extends Player {
+
+  /**
+   * Constructor
+   * @param color Player color: one of Constants.BLACK or Constants.WHITE
+   */
+  public MillerDrew(int color) {
+    super(color);
+  }
+
+  /**
+   *
+   * @param board
+   * @return The player's next move
+   */
+  @Override
+  public Position getNextMove(Board board) {
+     /* Your code goes here */
+  }
+}
